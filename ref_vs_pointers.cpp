f01b929d3d5071d64007ff010c02de07e999a0a6
CheesyPizza <gajansuryan@gmail.com>
2020-03-04 11:47:29 -0500

    Update ref_vs_pointers.cpp

--- ref_vs_pointers.cpp
+++ ref_vs_pointers.cpp
@@ -1,4 +1,18 @@
 // Example program
+/*
+A pointer can be re-assigned any number of times while a reference cannot be re-assigned after binding.
+Pointers can point nowhere (NULL), whereas a reference always refers to an object.
+You can't take the address of a reference like you can with pointers.
+There's no "reference arithmetic" (but you can take the address of an object pointed by a reference and do pointer arithmetic on it as in &obj + 5).
+To clarify a misconception:
+
+The C++ standard is very careful to avoid dictating how a compiler may implement references, but every C++ compiler implements references as pointers. That is, a declaration such as:
+
+int &ri = i;
+if it's not optimized away entirely, allocates the same amount of storage as a pointer, and places the address of i into that storage.
+  
+*/  
+  
 #include <iostream>
 #include <string>
 
